9cac02582c6921b6661a4d0ea67ff8ee03194352
the10or <the10or@gmail.com>
2024-02-18 22:32:13 +0200

    requirements

--- personal_assistant/personal_assistant/settings.py
+++ personal_assistant/personal_assistant/settings.py
@@ -26,7 +26,7 @@ environ.Env.read_env(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4v%!ynyv4if)v4f-$x9dm^lecx-4k3h1@c9*bci5w#&-p$8=er"
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
