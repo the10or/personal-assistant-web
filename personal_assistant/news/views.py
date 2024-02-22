49f0b7dd226042610e6a2c26f1c82adeb1618096
the10or <the10or@gmail.com>
2024-02-22 18:19:49 +0200

    move secrets to .env

--- personal_assistant/news/views.py
+++ personal_assistant/news/views.py
@@ -1,29 +1,28 @@
-from django.shortcuts import render
-
-# Create your views here.
 import json
-import random
 import urllib.request
-from pprint import pprint
 
+from django.conf import settings as s
 from django.shortcuts import render
 
-categories = ["general", "world", "nation", "business", "technology", "entertainment", "sports", "science", "health"]
+categories = [
+    "general",
+    "world",
+    "nation",
+    "business",
+    "technology",
+    "entertainment",
+    "sports",
+    "science",
+    "health"
+]
 
 
-# Create your views here.
 def index(request, category="nation"):
-    apikey = "8fa3ab2e35deefffc7007a3cc701766c"
+    apikey = s.NEWS_API_KEY
     category = category
-    url = f"https://gnews.io/api/v4/top-headlines?category={category}&lang=uk&country=ua&max=10&apikey={apikey}"
+    url = f"{s.NEWS_HOST}?category={category}&lang={s.NEWS_LANG}&country={s.NEWS_COUNTRY}&max=10&apikey={apikey}"
     with urllib.request.urlopen(url) as response:
         data = json.loads(response.read().decode("utf-8"))
         articles = data["articles"]
-        articles_on_page = []
-        for i in range(3):
-            ind = random.randint(0, len(articles) - 1)
-            article = articles[ind]
-            articles_on_page.append(article)
 
-
-    return render(request, "news/index.html", {"articles": articles_on_page, "categories": categories})
+    return render(request, "news/index.html", {"articles": articles, "categories": categories})
