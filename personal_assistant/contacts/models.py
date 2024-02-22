876afdbcca92d7d1b2e10fc39e3e4cf946cc2d0c
bubliq portable <anjey.mail@gmail.com>
2024-02-22 15:11:38 +0100

    user roles work, with login required

--- personal_assistant/contacts/models.py
+++ personal_assistant/contacts/models.py
@@ -8,5 +8,11 @@ class Contact(models.Model):
     email = models.EmailField()
     birthday = models.DateField()
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey("auth.User", on_delete=models.DO_NOTHING, related_name='created_contacts')
+    modified_at = models.DateTimeField(auto_now=True)
+    modified_by = models.ForeignKey("auth.User", on_delete=models.DO_NOTHING, related_name='modified_contacts')
+
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
