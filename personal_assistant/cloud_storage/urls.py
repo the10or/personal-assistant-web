2516ea3011679981411f41a76b697b6f1be4ee19
d43v30n <anjey.mail@gmail.com>
2024-02-27 18:31:11 +0100

    add cloud storage

--- personal_assistant/cloud_storage/urls.py
+++ personal_assistant/cloud_storage/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "cloud_storage"
+
 urlpatterns = [
-    path("", views.upload, name="upload"),
+    path("upload/", views.upload, name="upload"),
     path("list/", views.file_list, name="file_list"),
     path("list/<str:category>/", views.file_list, name="file_list_category"),
     path("delete/<int:file_id>/", views.delete_file, name="delete_file"),
