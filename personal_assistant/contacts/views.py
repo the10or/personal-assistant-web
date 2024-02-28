ee7c11188cc3b16982c2bacbbd3d3567bc32949d
bubliq portable <anjey.mail@gmail.com>
2024-02-28 11:49:21 +0100

    dbg

--- personal_assistant/contacts/views.py
+++ personal_assistant/contacts/views.py
@@ -137,4 +137,4 @@ def delete_contact(request, contact_id):
 def contact_details(request, contact_id: int):
     colleagues_ids = get_colleagues_ids(request)
     contact = Contact.objects.filter(id=contact_id, created_by__in=colleagues_ids).first()
-    return render(request, "contacts/note_details.html", {"contact": contact})
+    return render(request, "contacts/contact_details.html", {"contact": contact})
